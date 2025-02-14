13cb9363af2f47c980d0d0922dbc43b5dbb13cec
willmcroberts <williammcroberts15@gmail.com>
2025-02-14 15:48:51 +0000

    Started on main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <sstream>
+#include <fstream>
 
 int main() {
-	std::cout << "Hello World" << std::endl;
+	int num1;
+	int num2;
+	int sum;
+	std::string word;
+	std::ifstream inFile;
+	std::string currentLine;
+	std::stringstream converter;
+	std::stringstream ss;
+
+	inFile.open("data.csv");
+	
+	while (getline(inFile, currentLine)) {
+		converter.clear();
+		converter.str(currentLine);
+		getline(ss, currentLine, ',');
+		std::cout << currentLine << std::endl;
+	} // end while loop
 
 	return 0;
 } // end main
